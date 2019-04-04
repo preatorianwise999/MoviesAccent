5666ba2b63c81ff9dcd9db51f365060cf622e994
korbek100 <korbek100@gmail.com>
2019-04-04 17:06:18 -0300

    first commit

---
+++ Movs-Test/Controllers/VCFavorites.swift
@@ -0,0 +1,24 @@
+//
+//  VCFavorites.swift
+//  Movs-Test
+//
+//  Created by Jose David Bustos H on 04-04-19.
+//  Copyright © 2019 Jose David Bustos H. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class VCFavorites: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
---
+++ Movs-Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Movs-Test
+//
+//  Created by Jose David Bustos H on 03-04-19.
+//  Copyright © 2019 Jose David Bustos H. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
